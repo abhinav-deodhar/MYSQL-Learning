94ed2364a59d5f81a2864af3dad55872d68d2850
ABHINAV DEODHAR <abhinav.deodhar7@gmail.com>
2025-11-19 21:22:49 +0530

    Renamed the files to keep them in an order

---
+++ Day 1/01-DistinctClause.sql
@@ -0,0 +1,8 @@
+select * from Customers
+
+/*When we have repeated data in the same column and we want all the entries only once
+then we can use distinct keyword before the column name*/
+
+SELECT DISTINCT country
+from customers;
+
