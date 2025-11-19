dd84c9a3a9ccff0a110d07e7e94f3758d9d6cd8c
ABHINAV DEODHAR <abhinav.deodhar7@gmail.com>
2025-11-19 21:23:12 +0530

    Delete Day 1/DistinctClause.sql

--- Day 1/DistinctClause.sql
+++
@@ -1,8 +0,0 @@
-select * from Customers
-
-/*When we have repeated data in the same column and we want all the entries only once
-then we can use distinct keyword before the column name*/
-
-SELECT DISTINCT country
-from customers;
-
